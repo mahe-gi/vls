f406314579d4330dd76d68ffd9a839accef955af
Mahesh Ch <chmahesh.dev@gmail.com>
2025-08-03 00:29:44 +0530

    dsf

--- script.js
+++ script.js
@@ -30,3 +30,37 @@ const navMenu = document.getElementById("navMenu");
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+
+const callBtn = document.getElementById("bookConsuktationBtn");
+callBtn.addEventListener("click", () => {
+  window.open("https://wa.me/+919492864455?text=Hello%20World", "_blank");
+});
+
+const contactBtn = document.getElementById("contactBtn");
+contactBtn.addEventListener("click", () => {
+  window.open("tel:+919492864455");
+});
+
+const follower = document.querySelector(".cursor-follower");
+
+let mouseX = 0;
+let mouseY = 0;
+let posX = 0;
+let posY = 0;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function animate() {
+  posX += (mouseX - posX) * 0.1;
+  posY += (mouseY - posY) * 0.1;
+
+  follower.style.left = posX + "px";
+  follower.style.top = posY + "px";
+
+  requestAnimationFrame(animate);
+}
+
+animate();
